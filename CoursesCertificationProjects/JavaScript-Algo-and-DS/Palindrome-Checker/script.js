ce854e4e8ab7ca8fbf52081e51855276d5f12b65
Renold Thomas <renoldthomasp@gmail.com>
2025-08-01 09:31:14 -0400

    Made palindrome more appealing and user-friendly

--- CoursesCertificationProjects/JavaScript-Algo-and-DS/Palindrome-Checker/script.js
+++ CoursesCertificationProjects/JavaScript-Algo-and-DS/Palindrome-Checker/script.js
@@ -2,31 +2,44 @@ const resultText = document.getElementById('result');
 const textInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 
-checkButton.addEventListener('click', () => {
-  const inputValue = textInput.value;
-  if (!inputValue) {
-    alert('Please input a value');
-  } else {
-    const normalizeString = cleanString(inputValue);
-    const isPalindrome = checkPalindrome(normalizeString);
+// Add Enter key functionality
+textInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        checkButton.click();
+    }
+});
 
-    // if (isPalindrome) {
-    //   resultText.innerText = `${textInput.value} is a palindrome`;
-    // } else {
-    //   resultText.innerText = `${textInput.value} is not a palindrome`;
-    // }
-    resultText.innerText = isPalindrome
-        ? `${textInput.value} is a palindrome`
-        : `${textInput.value} is not a palindrome`;
-  }
+checkButton.addEventListener('click', () => {
+    const inputValue = textInput.value;
+    if (!inputValue) {
+        alert('Please input a value');
+    } else {
+        const normalizeString = cleanString(inputValue);
+        const isPalindrome = checkPalindrome(normalizeString);
+
+        // Remove previous classes
+        resultText.classList.remove('show-result', 'not-palindrome');
+
+        // Add appropriate classes with a small delay for animation
+        setTimeout(() => {
+            resultText.classList.add('show-result');
+            if (!isPalindrome) {
+                resultText.classList.add('not-palindrome');
+            }
+        }, 50);
+
+        resultText.innerText = isPalindrome
+            ? `${textInput.value} is a palindrome`
+            : `${textInput.value} is not a palindrome`;
+    }
 });
 
 const cleanString = (inputStr) => {
-  const alphanumericOnly = inputStr.replace(/[^a-zA-Z0-9]/gi, "");
-  const normalizedString = alphanumericOnly.toLowerCase();
-  return normalizedString;
+    const alphanumericOnly = inputStr.replace(/[^a-zA-Z0-9]/gi, "");
+    const normalizedString = alphanumericOnly.toLowerCase();
+    return normalizedString;
 }
 
 const checkPalindrome = (inputStr) => {
-  return inputStr === [...inputStr].reverse().join('');
+    return inputStr === [...inputStr].reverse().join('');
 }
